99ec647f77389491a469154ba84870f9db05baed
Yasir Uddin <yasir.uddin@icloud.com>
2021-11-21 13:30:38 -0700

    updated database name and base features still included

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,7 +3,7 @@ const Pool = pg.Pool;
 
 const config = {
     host: 'localhost',
-    database: 'tasks'
+    database: 'weekend-to-do-app'
 };
 
 const pool = new Pool(config);
@@ -11,6 +11,7 @@ const pool = new Pool(config);
 // pool.on('connect', () => {
 //     console.log('\tserver-database connection happened');
 // });
+
 
 pool.on('error', (poolError) => {
     console.log(poolError);
